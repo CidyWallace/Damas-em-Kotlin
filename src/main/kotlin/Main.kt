a4054102bd7068456a27d55f606354066c6f0a50
cidy <cidywallace12@gmail.com>
2025-07-06 17:32:07 -0300

    refatorando código

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ package org.example
 fun main() {
     var win = false
     var turn = true
-    val playerBranco = "⚫"
-    val playerPreto = "⚪"
+    val playerBranco = "⚪"
+    val playerPreto = "⚫"
     val casaBranca = " "
     val casaPreta = " "
     val board: Array<Array<String>> = buildBoard(playerBranco, playerPreto, casaBranca, casaPreta)
@@ -14,17 +14,18 @@ fun main() {
         showBoard(board)
         if(turn){
             println("$playerBranco Turn")
-            movimentoBranca(board, playerBranco, playerPreto, casaPreta)
+            verificaMovimento(board, playerBranco, casaBranca)
             turn = false
         }else{
             println("$playerPreto Turn")
-            movimentoPreta(board, playerPreto, playerBranco, casaPreta)
+            verificaMovimento(board, playerPreto, casaBranca)
             turn = true
         }
     }
 }
-fun movimentoPreta(board: Array<Array<String>>, playerPreto: String, playerBranco: String, casaBranca: String){
-    var choiseBoo = false
+
+fun verificaMovimento(board: Array<Array<String>>, mainPlayer: String, casaBranca: String): Boolean {
+    val secondPlayer = if(mainPlayer == "⚪") "⚫" else "⚪"
     var isEnemy = false
     val movimentacao: MutableMap<String, Pair<Int, Int>?> = mutableMapOf(
         "esquerda" to null,
@@ -34,95 +35,66 @@ fun movimentoPreta(board: Array<Array<String>>, playerPreto: String, playerBranc
         "esquerda" to null,
         "direita" to null
     )
-    val posicoes = posicao()
-    
-    while (!choiseBoo){
-        //Verifica as duas movimentações possiveis
-        if(board[posicoes.first.toInt()][posicoes.second.toInt()] == playerPreto){
-            //Esquerda com oponente
-            if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()-1][posicoes.second.toInt()-1] == playerBranco){
-                if(posicoes.first.toInt() - 2 >= 0 && posicoes.second.toInt() -2 >= 0 && board[posicoes.first.toInt()-2][posicoes.second.toInt()-2] == casaBranca){
-                    enemyPos["esquerda"] = posicoes.first.toInt()-1 to posicoes.second.toInt() - 1
-                    movimentacao["esquerda"] =posicoes.first.toInt()-2 to posicoes.second.toInt() - 2
-                    isEnemy = true
-                }
-//          Esquerda sem oponente
-            }else if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()-1][posicoes.second.toInt()-1] == casaBranca){
-                movimentacao["esquerda"] = posicoes.first.toInt()-1 to posicoes.second.toInt()-1
-            }
+    val posicoes = posicao(board, mainPlayer)
 
-            //Direita com oponente
-            if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()-1][posicoes.second.toInt()+1] == playerBranco){
-                if(posicoes.first.toInt() - 2 >= 0 && posicoes.second.toInt() + 2 <= 7 && board[posicoes.first.toInt()-2][posicoes.second.toInt()+2] == casaBranca){
-                    enemyPos["direita"] = posicoes.first.toInt()-1 to posicoes.second.toInt() + 1
-                    movimentacao["direita"] = posicoes.first.toInt()-2 to posicoes.second.toInt()+2
-                    isEnemy = true
-                }
-//          Direita sem inimigo
-            }else if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()-1][posicoes.second.toInt()+1] == casaBranca){
-                movimentacao["direita"] = posicoes.first.toInt() - 1 to posicoes.second.toInt() + 1
+    if(mainPlayer == "⚪"){
+        //    Esquerda com oponente
+        if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()+1][posicoes.second.toInt()-1] == secondPlayer){
+            if(posicoes.first.toInt() + 2 <= 7 && posicoes.second.toInt() -2 >= 0 && board[posicoes.first.toInt()+2][posicoes.second.toInt()-2] == casaBranca){
+                enemyPos["esquerda"] = posicoes.first.toInt()+1 to posicoes.second.toInt() - 1
+                movimentacao["esquerda"] =posicoes.first.toInt()+2 to posicoes.second.toInt() - 2
+                isEnemy = true
             }
+//    Esquerda sem oponente
+        }else if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()+1][posicoes.second.toInt()-1] == casaBranca){
+            movimentacao["esquerda"] = posicoes.first.toInt()+1 to posicoes.second.toInt()-1
+        }
 
-            if(verificaMovimentacao(board, movimentacao, enemyPos, posicoes.first.toInt(), posicoes.second.toInt(), playerPreto, isEnemy, casaBranca)){
-                choiseBoo = true
+//    Direita com oponente
+        if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()+1][posicoes.second.toInt()+1] == secondPlayer){
+            if(posicoes.first.toInt() + 2 <= 7 && posicoes.second.toInt() + 2 <= 7 && board[posicoes.first.toInt()+2][posicoes.second.toInt()+2] == casaBranca){
+                enemyPos["direita"] = posicoes.first.toInt()+1 to posicoes.second.toInt() + 1
+                movimentacao["direita"] = posicoes.first.toInt()+2 to posicoes.second.toInt()+2
+                isEnemy = true
             }
+//     Direita sem inimigo
+        }else if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()+1][posicoes.second.toInt()+1] == casaBranca){
+            movimentacao["direita"] = posicoes.first.toInt() + 1 to posicoes.second.toInt() + 1
+        }
+    }else{
+        //    Esquerda com oponente
+        if(posicoes.first.toInt() + 1 >= 0 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()-1][posicoes.second.toInt()-1] == secondPlayer){
+            if(posicoes.first.toInt() + 2 >= 0 && posicoes.second.toInt() -2 >= 0 && board[posicoes.first.toInt()-2][posicoes.second.toInt()-2] == casaBranca){
+                enemyPos["esquerda"] = posicoes.first.toInt()-1 to posicoes.second.toInt() - 1
+                movimentacao["esquerda"] =posicoes.first.toInt()-2 to posicoes.second.toInt() - 2
+                isEnemy = true
+            }
+//    Esquerda sem oponente
+        }else if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()-1][posicoes.second.toInt()-1] == casaBranca){
+            movimentacao["esquerda"] = posicoes.first.toInt()-1 to posicoes.second.toInt()-1
+        }
 
-        }else{
-            println("Não existe uma peça válida nesta posição. Tá Loco?")
+//    Direita com oponente
+        if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() - 1 <= 7 && board[posicoes.first.toInt()-1][posicoes.second.toInt()+1] == secondPlayer){
+            if(posicoes.first.toInt() - 2 >= 0 && posicoes.second.toInt() - 2 <= 7 && board[posicoes.first.toInt()-2][posicoes.second.toInt()+2] == casaBranca){
+                enemyPos["direita"] = posicoes.first.toInt()-1 to posicoes.second.toInt() + 1
+                movimentacao["direita"] = posicoes.first.toInt()-2 to posicoes.second.toInt()+2
+                isEnemy = true
+            }
+//     Direita sem inimigo
+        }else if(posicoes.first.toInt() - 1 >= 0 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()-1][posicoes.second.toInt()+1] == casaBranca){
+            movimentacao["direita"] = posicoes.first.toInt() - 1 to posicoes.second.toInt() + 1
         }
     }
-}
 
-fun movimentoBranca(board: Array<Array<String>>, playerBranco: String, playerPreto: String, casaBranca: String){
-    var choiseBoo = false
-    var isEnemy = false
-    val movimentacao: MutableMap<String, Pair<Int, Int>?> = mutableMapOf(
-        "esquerda" to null,
-        "direita" to null
-    )
-    val positionEnemy: MutableMap<String, Pair<Int, Int>?> = mutableMapOf(
-        "esquerda" to null,
-        "direita" to null
-    )
-
-    while (!choiseBoo){
-        val posicoes = posicao()
-
-        //Verifica as duas movimentações possiveis
-        //Esquerda
-        if(board[posicoes.first.toInt()][posicoes.second.toInt()] == playerBranco){
-            if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()+1][posicoes.second.toInt()-1] == playerPreto){
-                if(posicoes.first.toInt() + 2 <= 7 && posicoes.second.toInt() - 2 >= 0 && board[posicoes.first.toInt()+2][posicoes.second.toInt()-2] == casaBranca){
-                    positionEnemy["esquerda"] = posicoes.first.toInt()+1 to posicoes.second.toInt() - 1
-                    movimentacao["esquerda"] = posicoes.first.toInt()+2 to posicoes.second.toInt() - 2
-                    isEnemy = true
-                }
-
-            }else if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() - 1 >= 0 && board[posicoes.first.toInt()+1][posicoes.second.toInt()-1] == casaBranca){
-                movimentacao["esquerda"] = posicoes.first.toInt()+1 to posicoes.second.toInt()-1
-            }
-            //Direita
-            if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()+1][posicoes.second.toInt()+1] == playerPreto){
-                if(posicoes.first.toInt() + 2 <= 7 && posicoes.second.toInt() + 2 <= 7 && board[posicoes.first.toInt()+2][posicoes.second.toInt()+2] == casaBranca){
-                    positionEnemy["direita"] = posicoes.first.toInt()+1 to posicoes.second.toInt() + 1
-                    movimentacao["direita"] = posicoes.first.toInt()+2 to posicoes.second.toInt() + 2
-                    isEnemy = true
-                }
-            }else if(posicoes.first.toInt() + 1 <= 7 && posicoes.second.toInt() + 1 <= 7 && board[posicoes.first.toInt()+1][posicoes.second.toInt()+1] == casaBranca){
-                movimentacao["direita"] = posicoes.first.toInt() + 1 to posicoes.second.toInt() + 1
-            }
-
-            if(verificaMovimentacao(board, movimentacao,positionEnemy, posicoes.first.toInt(), posicoes.second.toInt(), playerBranco, isEnemy, casaBranca)){
-                choiseBoo = true
-            }
-
-        }else{
-            println("Não existe uma peça válida nesta posição. Tá Loco?")
-        }
+    if(movimentacaoPecas(board, movimentacao, enemyPos, posicoes.first.toInt(), posicoes.second.toInt(), mainPlayer, isEnemy, casaBranca)){
+        return true
     }
+
+    return false
 }
 
-fun verificaMovimentacao(board: Array<Array<String>>, movimentacao: MutableMap<String, Pair<Int, Int>?>, enemyPosition: MutableMap<String, Pair<Int, Int>?>, linha:Int, coluna:Int, player: String, isEnemy: Boolean, casaBranca: String): Boolean{
+fun movimentacaoPecas(board: Array<Array<String>>, movimentacao: MutableMap<String, Pair<Int, Int>?>, enemyPosition: MutableMap<String, Pair<Int, Int>?>, linha:Int, coluna:Int, player: String, isEnemy: Boolean, casaBranca: String): Boolean{
 
     if(movimentacao["esquerda"] != null || movimentacao["direita"] != null) {
         val esquerda = movimentacao["esquerda"]
@@ -205,7 +177,6 @@ fun showBoard(board: Array<Array<String>>) {
 fun buildBoard(playerBranco: String, playerPreto: String, casaBranca: String, casaPreta: String): Array<Array<String>> {
     val tabuleiro = Array (8) { Array (8) { " " } }
 
-    //Posicionar peças brancas
     for(i in 0 until 8){
         for(j in 0 until 8){
             val isCasaPreta = (i + j) % 2 != 0
@@ -218,29 +189,13 @@ fun buildBoard(playerBranco: String, playerPreto: String, casaBranca: String, ca
             }else{
                 tabuleiro[i][j] = casaBranca
             }
-//            if((i == 0 || i == 2) && j % 2 != 0){
-//                tabuleiro[i][j] = "B"
-//            }else if(i == 1 && j % 2 == 0){
-//                tabuleiro[i][j] = "B"
-//            }
         }
     }
-
-//    // Posicionar peças pretas
-//    for(i in 7 downTo 5){
-//        for(j in 0..7){
-//            if((i == 7 || i == 5) && j % 2 == 0){
-//                tabuleiro[i][j] = "P"
-//            }else if(i == 6 && j % 2 != 0){
-//                tabuleiro[i][j] = "P"
-//            }
-//        }
-//    }
 
     return tabuleiro
 }
 
-fun posicao(): Pair<String, String>{
+fun posicao(board: Array<Array<String>>, player: String): Pair<String, String>{
     val pass = false
 
     do{
@@ -250,8 +205,12 @@ fun posicao(): Pair<String, String>{
         val coluna = readLine().toString()
 
         if(linha.contains(regex = Regex("[0-9]+")) && coluna.contains(regex = Regex("[0-9]+")) && !linha.isEmpty() && !coluna.isEmpty()){
-            val result = linha to coluna
-            return result
+            if(board[linha.toInt()][coluna.toInt()] == player){
+                val result = linha to coluna
+                return result
+            }else{
+                println("Não existe peças válidas nesta posição do tabuleiro!")
+            }
         }
     }while (!pass)
     return "" to ""
